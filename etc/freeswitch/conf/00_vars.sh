63e0dce72f07727705297e2149718099953ce10a
Darius Stefan <darius.stefan@opensips.org>
2025-01-20 15:28:23 +0200

    Remove freeswitch service

--- etc/freeswitch/conf/00_vars.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-eval "sed -E -i 's/external_sip_ip=[^\"]+/external_sip_ip=${FREESWITCH_IP}/' /etc/freeswitch/vars.xml"
-eval "sed -E -i 's/external_rtp_ip=[^\"]+/external_rtp_ip=${FREESWITCH_IP}/' /etc/freeswitch/vars.xml"
