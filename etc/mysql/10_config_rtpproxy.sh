0776bf19fdb800e4512ef82ec49c310c91ead531
Darius Stefan <darius.stefan@opensips.org>
2025-01-21 14:38:54 +0200

    Try to connect opensips and rtpengine

--- etc/mysql/10_config_rtpproxy.sh
+++ etc/mysql/10_config_rtpproxy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Configuring OpenSIPS CP RTPProxy Tool ..."
+echo "Configuring OpenSIPS CP RTPEngine Tool ..."
 
 mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e \
-	"INSERT INTO rtpproxy_sockets (rtpproxy_sock, set_id)
-	 SELECT 'udp:${RTPPROXY_IP}:${RTPPROXY_PORT}', 0
+	"INSERT INTO rtpengine (socket, set_id)
+	 SELECT 'udp:${RTPENGINE_IP}:${RTPENGINE_PORT}', 0
 		 WHERE NOT EXISTS (
-		 SELECT 1 FROM rtpproxy_sockets WHERE rtpproxy_sock = 'udp:${RTPPROXY_IP}:${RTPPROXY_PORT}');"
+		 SELECT 1 FROM rtpengine WHERE socket = 'udp:${RTPENGINE_IP}:${RTPENGINE_PORT}');"
 
