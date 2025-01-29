59e1adf31a5653da86cf8815cd7ff157699d5dfb
Darius Stefan <darius.stefan@opensips.org>
2025-01-29 15:09:52 +0200

    Add dialplan_did table and remove dids

---
+++ etc/mysql/21_create_dialplan_did.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Configuring Dialplan DID Table ..."
+
+TABLE_EXISTS=$(mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" -e \
+    "SELECT 1 FROM information_schema.tables WHERE table_schema = '${MYSQL_DATABASE}' AND table_name = 'dialplan_did';")
+
+if [ -n "$TABLE_EXISTS" ]; then
+    echo "Table dialplan_did already exists"
+else
+    echo "Creating dialplan_did table"
+
+    TABLE_VERSION=$(mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" -se \
+        "SELECT table_version FROM version WHERE table_name = 'dialplan';")
+
+    mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" -e \
+        "INSERT INTO version (table_name, table_version) values ('dialplan_did', '${TABLE_VERSION}');"
+
+    mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" -e \
+        "CREATE TABLE dialplan_did LIKE dialplan;"
+fi
