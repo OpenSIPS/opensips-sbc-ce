e63568ee7b75cc8a1bf16caa2c596150f27e7fbd
Darius Stefan <darius.stefan@opensips.org>
2025-01-29 15:09:12 +0200

    Move dialplan-sip table creation in mysql init folder

--- etc/mysql/20_create_dialplan_sip.sh
+++ etc/mysql/20_create_dialplan_sip.sh
@@ -9,8 +9,12 @@ if [ -n "$TABLE_EXISTS" ]; then
     echo "Table dialplan_sip already exists"
 else
     echo "Creating dialplan_sip table"
+
+    TABLE_VERSION=$(mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" -se \
+        "SELECT table_version FROM version WHERE table_name = 'dialplan';")
+
     mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" -e \
-        "INSERT INTO version (table_name, table_version) values ('dialplan_sip','1');"
+        "INSERT INTO version (table_name, table_version) values ('dialplan_sip', '${TABLE_VERSION}');"
 
     mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" -e \
         "CREATE TABLE dialplan_sip LIKE dialplan;"
