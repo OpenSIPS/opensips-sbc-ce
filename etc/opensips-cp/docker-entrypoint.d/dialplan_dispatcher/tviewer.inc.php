211fb3f5d113eaf6b4041e1752e5d769ccc9914b
Stefan Darius <darius.stefan@opensips.org>
2026-04-20 13:15:40 +0300

    Fix dpid validation failed

--- etc/opensips-cp/docker-entrypoint.d/dialplan_dispatcher/tviewer.inc.php
+++ etc/opensips-cp/docker-entrypoint.d/dialplan_dispatcher/tviewer.inc.php
@@ -122,6 +122,7 @@ $custom_config[$module_id][0]['custom_table_column_defs'] = array (
 	"dpid" 	=> 	array (
 		"header" 			=> "Dialplan Group ID",
 		"type"				=> "text",
+		"is_optional"			=> "y",
 		"show_in_add_form" 		=> false,
 		"show_in_edit_form"		=> false,
 		"searchable" 			=> false,
