79ea3a43c03ef57e2c0ade516163d796aa567ed1
Stefan Darius <darius.stefan@opensips.org>
2026-04-20 18:01:42 +0300

    Update CP config db script

--- etc/opensips-cp/docker-entrypoint.d/00_config_db.sh
+++ etc/opensips-cp/docker-entrypoint.d/00_config_db.sh
@@ -7,6 +7,6 @@ echo "Configuring OpenSIPS-CP database connection..."
 
 sed -i "s/localhost/${MYSQL_IP}/g" /var/www/html/opensips-cp/config/db.inc.php
 
-sed -i "s/127.0.0.1/${OPENSIPS_IP}/g" /var/www/html/opensips-cp/config/db_schema.mysql
+sed -i "s/127.0.0.1/${HOST_IP}/g" /var/www/html/opensips-cp/config/db_schema.mysql
 
 add_table /var/www/html/opensips-cp/config/db_schema.mysql
